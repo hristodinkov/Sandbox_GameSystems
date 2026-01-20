e2ba26edcbc3db24eac183183bcf8e9a0c2104a0
Icaka_XD <hristodincov@gmail.com>
2026-01-21 00:12:43 +0100

    adding the effects to the customer

---
+++ Assets/Script/CocktailReceiver.cs
@@ -0,0 +1,223 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CocktailReceiver : MonoBehaviour
+{
+    [SerializeField] private bool fireBreathingEnabled;
+    [SerializeField] private bool iceCubeEnabled;
+    [SerializeField] private bool chickenEffectEnabled;
+    [SerializeField] private bool fartEnabled;
+    [SerializeField] private bool shrinkEffectEnabled;
+    [SerializeField] private bool hiccupEffectEnabled;
+    [SerializeField] private bool helicopterEffectEnabled;
+
+    [SerializeField] private ParticleSystem fireBreathEffect;
+    [SerializeField] private GameObject iceCube;
+    [SerializeField] private GameObject chickenModel;
+    [SerializeField] private ParticleSystem fartEffect;
+    [SerializeField] private ParticleSystem chickenTransformation;
+    [SerializeField] private GameObject theBoss;
+    [SerializeField] private GameObject theWholeGameObject;
+    [SerializeField] private Animator bossAnimator;
+
+    [SerializeField] private float speedRotation;
+
+    private Coroutine hiccupRoutine;
+
+
+    private DrinkEffectType[] receivedEffects;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cocktail"))
+        {
+            ResetUnc();
+            var cocktail = other.GetComponent<CocktailData>();
+
+            receivedEffects = cocktail.effects.ToArray();
+
+            ReadCocktailInfo();
+            ApplyEffects();
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (helicopterEffectEnabled)
+        {
+            theWholeGameObject.transform.Rotate(0, 300f * Time.deltaTime*speedRotation, 0);
+        }
+        else
+        {
+            theWholeGameObject.transform.rotation = Quaternion.identity;
+        }
+    }
+
+
+    //private void Update()
+    //{
+    //    ApplyEffects();
+    //}
+
+    private void ApplyEffects()
+    {
+        FireBreathing();
+        IceCube();
+        Fart();
+        Chicken();
+        Shrink();
+        HandleHiccup();
+    }
+
+    private void HandleHiccup()
+    {
+        if (hiccupEffectEnabled)
+        {
+            if (hiccupRoutine == null)
+                hiccupRoutine = StartCoroutine(HiccupLoop());
+        }
+        else
+        {
+            if (hiccupRoutine != null)
+            {
+                StopCoroutine(hiccupRoutine);
+                hiccupRoutine = null;
+            }
+        }
+    }
+    private IEnumerator HiccupLoop()
+    {
+        while (hiccupEffectEnabled)
+        {
+            // Hiccup animation
+            theWholeGameObject.transform.position += new Vector3(0, 0.5f, 0);
+            yield return new WaitForSeconds(0.2f);
+            theWholeGameObject.transform.position -= new Vector3(0, 0.5f, 0);
+
+            // Wait 4 seconds before next hiccup
+            yield return new WaitForSeconds(4f);
+        }
+
+        hiccupRoutine = null;
+    }
+
+
+
+    private IEnumerator Hiccup()
+    {
+        if (hiccupEffectEnabled)
+        {
+            theWholeGameObject.transform.position += new Vector3(0, 0.5f, 0);
+            yield return new WaitForSeconds(0.2f);
+            theWholeGameObject.transform.position -= new Vector3(0, 0.5f, 0);
+            yield return new WaitForSeconds(4f);
+        }
+        
+    }
+
+    private void Shrink()
+    {
+        if(shrinkEffectEnabled)
+        {
+            theWholeGameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            theWholeGameObject.transform.position += new Vector3(0.7f, 0f, 0);
+
+        }
+        else
+        {
+            theWholeGameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            theWholeGameObject.transform.position -= new Vector3(0.7f, 0f, 0);
+        }
+    }
+
+    private void Fart()
+    {
+        if (fartEnabled)
+        {
+            if (!fartEffect.isPlaying)
+                fartEffect.Play();
+        }
+        else
+        {
+            fartEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+    }
+
+
+    private void Chicken()
+    {
+        if (chickenEffectEnabled)
+        {
+            chickenModel.SetActive(true);
+            theBoss.SetActive(false);
+
+            // Play transformation effect
+            chickenTransformation.Play();
+        }
+        else
+        {
+            chickenModel.SetActive(false);
+            theBoss.SetActive(true);
+
+            // Stop but DO NOT disable the GameObject
+            chickenTransformation.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+    }
+
+
+    private void IceCube()
+    {
+        // Ice cube
+        iceCube.SetActive(iceCubeEnabled);
+        bossAnimator.enabled = !iceCubeEnabled;
+    }
+
+    private void FireBreathing()
+    {
+        if (fireBreathingEnabled)
+        {
+            if (!fireBreathEffect.isPlaying)
+                fireBreathEffect.Play();
+        }
+        else
+        {
+            fireBreathEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+    }
+
+
+    private void ResetUnc()
+    {
+        fireBreathingEnabled = false;
+        iceCubeEnabled = false;
+        chickenEffectEnabled = false;
+        fartEnabled = false;
+        shrinkEffectEnabled = false;
+        hiccupEffectEnabled = false;
+
+        if (hiccupRoutine != null)
+        {
+            StopCoroutine(hiccupRoutine);
+            hiccupRoutine = null;
+        }
+
+        theWholeGameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        theWholeGameObject.transform.Rotate(0, 0f, 0);
+    }
+   
+
+    private void ReadCocktailInfo()
+    {
+        fireBreathingEnabled = receivedEffects.Contains(DrinkEffectType.FireBreath);
+        iceCubeEnabled = receivedEffects.Contains(DrinkEffectType.IceCube);
+        chickenEffectEnabled = receivedEffects.Contains(DrinkEffectType.Chicken);
+        fartEnabled = receivedEffects.Contains(DrinkEffectType.Fart);
+        shrinkEffectEnabled = receivedEffects.Contains(DrinkEffectType.Shrink);
+        hiccupEffectEnabled = receivedEffects.Contains(DrinkEffectType.Hiccup);
+        helicopterEffectEnabled = receivedEffects.Contains(DrinkEffectType.Helicopter);
+    }
+}
